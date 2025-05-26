2372a42c84ac55220b6a33eae425b1be7f9e96fc
janalam <janalam1224@gmail.com>
2025-05-26 16:52:40 +0500

    src/schemas/schemas Added

---
+++ src/schemas/schemas.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const signupSchema = z.object({
+  name: z.string().trim(),
+  email: z.string().trim().email(),
+  password: z.string().trim().min(5).max(20),
+  userType: z.string().trim(),
+});
+
+export const loginSchema = z.object({
+  email: z.string().trim().email(),
+  password: z.string().trim().min(5).max(20),
+});
+
+export const createProdSchema = z.object({
+  name: z.string().min(1, "Product name is required"),
+  description: z.string().min(1, "Description is required"),
+  price: z.number().positive("Price must be a positive number"),
+  category: z.string().min(1, "Category is required"),
+  stock: z.number().int().nonnegative("Stock must be a non-negative integer"),
+  rating: z.number().min(0).max(5).optional(), // Rating is usually 0-5
+  image: z.string().url("Image must be a valid URL")
+});
