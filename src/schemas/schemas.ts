f455ec2c2c6a31b70e83e350e54ecf6b063fc987
janalam <janalam1224@gmail.com>
2025-07-16 13:00:32 +0500

    Modified schemas

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -17,20 +17,19 @@ password: z.coerce.string().trim().min(5, "Password must be at least 5 character
 });
 
 export const createProductSchema = z.object({
-  images: z.array(z.string().url()),
+  images: z.array(z.string().url()).optional(),
   name: z.string(),
   reference: z.string(),
-  barcode: z.number().optional(),
-  discountedPrice: z.number().optional(),
-  cost: z.number(),
-  price: z.number(),
-  salePrice: z.number(),
-  tax: z.number(),
-  stock: z.number(),
-  categoryName:z.string(),
-  status: z.enum(['active', 'inactive']).default('active'),
-  createdAt: z.string().datetime(),
-
+  barcode: z.coerce.number().nullable().optional(),
+  discountedPrice: z.coerce.number().nullable().optional(),
+  cost: z.coerce.number(),
+  price: z.coerce.number(),
+  salePrice: z.coerce.number().nullable().optional(),
+  tax: z.coerce.number(),
+  stock: z.coerce.number(),
+  categoryId: z.coerce.number(),
+  status: z.enum(['active', 'inactive']),
+  createdAt: z.string().datetime().optional(),
 });
 
 export const createCategorySchema = z.object({
@@ -72,40 +71,10 @@ export const createOrderSchema = z.object({
   ).min(1, 'At least one item is required'),
 });
 
-export const createBillSchema = z.object({
-  orderId: z.string(),
-  userId: z.string(),
-  discount: z.number().optional()
-});
-
-export const createTransactionSchema = z.object({
-  orderId: z.string().min(1, 'Order ID is required'),
-  userId: z.string().min(1, 'User ID is required'),
-  amount: z.number().min(0, 'Amount must be a non-negative number'),
-  paymentMethod: z.enum(['Stripe', 'PayPal', 'COD', 'Razorpay', 'Other']),
-  status: z.enum(['Success', 'Failed']),
-});
-
 export const createCartItemSchema = z.object({
   userId: z.string().min(1, 'User ID is required'),
   productId: z.string().min(1, 'Product ID is required'),
   quantity: z.number().int().positive('Quantity must be a positive integer'),
 });
 
-export const createReviewSchema = z.object({
-  userId: z.string().min(1, 'User ID is required'),
-  productId: z.string().min(1, 'Product ID is required'),
-  rating: z.number()
-  .min(1, 'Rating must be at least 1')
-  .max(5, 'Rating cannot exceed 5'),
-  comment: z.string().optional(),
-});
 
-export const notificationSchema = z.object({
-  vendorId: z.string().min(1, 'Vendor ID is required'),
-  type: z.enum(['low_stock_alert', 'order_status', 'new_review', 'general']),
-  productId: z.string().min(1, 'Product ID is required'),
-  message: z.string().min(1, 'Message is required'),
-  createdAt: z.string().datetime({ message: 'Invalid ISO date format' }),
-  read: z.boolean()
-});
