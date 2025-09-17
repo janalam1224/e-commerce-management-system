a1e367f21f9b6fb88f1cc556bdd1adc2ec16a119
janalam <janalam1224@gmail.com>
2025-09-17 14:39:21 +0500

    Modify app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,24 @@
-import dotenv from 'dotenv';
-import express from 'express';
-dotenv.config();
+import express from "express";
+import setupSwagger from "../config/swagger";
 
-import authRouter from './routes/authRouter';
-import userRouter from './routes/userRouter';
-import productRouter from './routes/productRouter';
-import categoryRouter from './routes/categoryRouter';
-import orderRouter from './routes/orderRouter';
-
-dotenv.config();
+import authRouter from "./routes/authRouter";
+import userRouter from "./routes/userRouter";
+import productRouter from "./routes/productRouter";
+import categoryRouter from "./routes/categoryRouter";
+import orderRouter from "./routes/orderRouter";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/auth', authRouter);
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/categories', categoryRouter);
-app.use('/api/orders', orderRouter);
+setupSwagger(app);
+
+app.use("/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/categories", categoryRouter);
+app.use("/api/orders", orderRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
